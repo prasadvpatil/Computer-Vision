43c75009b557b163e6c1bd2567ab813dca16f6dc
Saloni Shah <saloshah01@gmail.com>
2020-10-01 18:24:11 +0530

    Update pencilsketch.py

--- pencilsketch.py
+++ pencilsketch.py
@@ -1,7 +1,7 @@
 import cv2
 def dodgeV2(x, y):
     return cv2.divide(x, 255 - y, scale=256)
-img = cv2.imread('capture.png',1)
+img = cv2.imread('capture.png',1)   #add an image to the folder where you save this code.
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_invert = cv2.bitwise_not(img_gray)
 img_smoothing = cv2.GaussianBlur(img_invert, (21, 21),sigmaX=0, sigmaY=0)
